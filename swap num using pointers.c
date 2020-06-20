78df4eeb8c629bd6af4d182083afda38185008b3
hemalatha36 <44522100+hemalatha36@users.noreply.github.com>
2020-06-20 09:03:12 +0530

    Add files via upload

---
+++ swap num using pointers.c
@@ -0,0 +1,24 @@
+C Program to swap the numbers using pointers
+
+#include <stdio.h>
+ 
+int main()
+{
+   int x, y, *a, *b, temp;
+ 
+   printf("Enter the value of x and y\n");
+   scanf("%d%d", &x, &y);
+ 
+   printf("Before Swapping\nx = %d\ny = %d\n", x, y);
+ 
+   a = &x;
+   b = &y;
+ 
+   temp = *b;
+   *b = *a;
+   *a = temp;
+ 
+   printf("After Swapping\nx = %d\ny = %d\n", x, y);
+ 
+   return 0;
+}
